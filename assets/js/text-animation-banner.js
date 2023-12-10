40bc779ace5559d6c6c40a8a9d6da38e4370c699
Minhaj AT <minhajatholi222@gmail.com>
2023-12-10 23:22:41 +0530

    banner animation

---
+++ assets/js/text-animation-banner.js
@@ -0,0 +1,21 @@
+// const titles = gsap.utils.toArray('.skills-text');
+//     const tl = gsap.timeline();
+
+//     titles.forEach((title, index) => {
+//       const splitTitle = new SplitType(title, { types: 'chars, words' });
+
+
+//       tl.from(splitTitle.chars, { 
+//         y:40,
+//         stagger: 0.02,
+//         rotateX:-90,
+//     }, "<");
+  
+//     tl.to(splitTitle.chars, { 
+//         y:-40,
+//         rotateX:90,
+//         stagger: 0.02,  
+//     }, "<2");
+      
+      
+//     });
